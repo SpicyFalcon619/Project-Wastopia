435c9d7c2c145f490054cefabbec307c16b80dc3
Ahmad Maruf Hossain <hossainmysterio619@gmail.com>
2026-06-03 19:09:00 +0600

    Fix hydration warning on html tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${poppins.variable} ${jetbrainsMono.variable}`}>
+    <html lang="en" className={`${poppins.variable} ${jetbrainsMono.variable}`} suppressHydrationWarning>
       <head>
         <script
           dangerouslySetInnerHTML={{
